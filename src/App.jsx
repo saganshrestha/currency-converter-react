90063e06186ca2123d56780d634f41c16109e728
Sagan Shrestha <saganshrestha01@gmail.com>
2024-11-23 16:10:50 +0545

    added functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-
-import React from 'react'
-import ConverterForm from './Components/ConverterForm'
+import React from "react";
+import ConverterForm from "./Components/ConverterForm";
 
 const App = () => {
   return (
-    <div className='p-10 rounded-lg main'>
-      <h2 className='mb-10 text-2xl font-bold tracking-wider text-center text-white'>Currency Converter</h2>
+    <div className="p-10 rounded-lg main">
+      <h2 className="mb-10 text-2xl font-bold tracking-wider text-center text-white">
+        Currency Converter
+      </h2>
       <ConverterForm />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
